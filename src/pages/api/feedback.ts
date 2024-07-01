a46679937ee367152b68c47c06d9acb567b7307b
Ruslan <Giovingo92@gmail.com>
2024-07-01 15:10:59 +0300

    fix: undefined

--- src/pages/api/feedback.ts
+++ src/pages/api/feedback.ts
@@ -11,15 +11,23 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     const newFeedback = {
       id: new Date().toISOString(),
       email: email,
-      text: feedbackText 
+      text: feedbackText,
     };
 
     const filePath = path.join(process.cwd(), "data", "feedback.json");
-    const fileData = fs.readFileSync(filePath, "utf8");
-    const data = JSON.parse(fileData);
+
+    let data = [];
+    
+    try {
+      const fileData = fs.readFileSync(filePath, "utf8");
+      data = JSON.parse(fileData);
+    } catch (error) {
+      console.log(error)
+    }
+
     data.push(newFeedback);
     fs.writeFileSync(filePath, JSON.stringify(data));
-    res.status(201).json({ message: "Success!", feedBack: newFeedback });
+    res.status(201).json({ message: "Success!", feedback: newFeedback });
   } else {
     res.status(200).json({ message: "This works!" });
   }
